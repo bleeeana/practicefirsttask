79d32af51c97983108492930184d66c6b0a41aba
bleeeana <bobkov.v03@icloud.com>
2023-07-03 09:51:26 +0300

    lb1

--- src/firsttask.kt
+++ src/firsttask.kt
@@ -1,6 +1,6 @@
 package org.jetbrains.kotlin.Math
 val scan = java.util.Scanner(System.`in`)
-
+// реализация класса изменяемого внутри функций целого числа
 class MutableInt(var value : Int){
     fun get() : Int{
         return value
@@ -39,7 +39,7 @@ open class Vector<type_name : Any>{
         }
         return current?.value
     }
-
+// перегруженный метод для изменения значения на value в списке по индексу index
     operator fun set(index : Int, value : type_name){
         if (index >=0){
             var current = head
@@ -56,7 +56,7 @@ open class Vector<type_name : Any>{
             }
         }
     }
-
+// метод для определения размера списка
     fun size() : Int{
         var current = head
         var size = 0
@@ -66,7 +66,7 @@ open class Vector<type_name : Any>{
         }
         return size
     }
-
+// метод для определения индекса элемента со значением value в списке
     fun index(value : type_name) : Int{
         var current = head
         for (i in 0 until size()){
@@ -78,7 +78,7 @@ open class Vector<type_name : Any>{
         }
         return -1
     }
-
+// метод для удаления элемента по индексу index 
     fun delete_element(index : Int){
         if (index in 0 until size()) {
             if (index == 0) {
@@ -92,7 +92,7 @@ open class Vector<type_name : Any>{
             }
         }
     }
-
+// функция для преобразования списка в строку
     override fun toString() : String {
         var current = this.head
         var string : String = ""
@@ -107,25 +107,33 @@ open class Vector<type_name : Any>{
         }
         return string
     }
-
+// метод для удаления всего списка
     fun clear() {
         for (i in size() downTo 0){
             this.delete_element(i)
         }
     }
 }
+/*класс для представления структуры квадрата на столе
+имеет 3 характеристики: координата по х верхнего левого угла
+                        координата по у верхнего левого угла
+                        длина стороны квадрата size
+ */ 
+
 open class Square(var x : Int, var y : Int, var size : Int){
+    // функция для вывода в качестве строки информации о конкретном квадрате
     override fun toString(): String {
         return "${x + 1} ${y + 1} $size"
     }
-
+    // функция для умножения всех характеристик квадрата на коэффициент при обратном масштабировании
     fun get_first_size(new_N : Int, old_N : Int){
         this.x *= (old_N / new_N)
         this.y *= (old_N / new_N)
         this.size *= (old_N / new_N)
     }
 }
-
+// функция, нужная для нахождения коэффициента, равного наименьшего простого делителя
+// нужна в качестве оптимизации, чтобы вместо стола 15*15 собирать стол 3*3, что будет гораздо быстрее и будет иметь такой же правильный ответ
 fun find_koefficient(n : Int) : Int{
     for (i in 2 until n){
         if (n % i == 0){
@@ -134,7 +142,7 @@ fun find_koefficient(n : Int) : Int{
     }
     return n
 }
-
+//фукнция для копирования одного списка в другой(other_table -> new_table)
 fun clone(other_table : Vector<Square>, new_table : Vector<Square>){
     var current = other_table.head
     for (i in 0 until other_table.size()){
@@ -143,13 +151,14 @@ fun clone(other_table : Vector<Square>, new_table : Vector<Square>){
         current = current.next_element
     }
 }
-
+// функция, которая для каждого квадрата в столе вызывает метод get_firt_size
 fun get_first_size(table : Vector<Square>, new_N: Int, old_N: Int){
     for (i in 0 until table.size()){
         table.get(i)?.get_first_size(new_N, old_N)
     }
 }
-
+// функция, которая проверяет, находится ли точка на столе внутри какого-либо квадрата, уже имеющимся на данном столе
+// возвращает true , если точка находится внутри, false — если нет
 fun is_inside_old_squares(table : Vector<Square>,new_x : Int, new_y : Int) : Boolean{
     for (i in 0 until table.size()){
         if (new_x >= table.get(i)?.x!! && new_x < table.get(i)?.x!! + table.get(i)?.size!! && new_y >= table.get(i)?.y!! && new_y < table.get(i)?.y!! + table.get(i)?.size!!){
@@ -158,7 +167,11 @@ fun is_inside_old_squares(table : Vector<Square>,new_x : Int, new_y : Int) : Boo
     }
     return false
 }
-
+/* функция, совершающая по принципу бектрекинга перебор всех возможных сборок стола 
+передвижение по столу совершается сверху вниз, выбирается наибольшая сторона квадрата, левый верхний угол которого нааходится в свободной точке
+затем идет перебор от большего к меньшему всех вариантов вставки квадрата в эту точку
+присутствует оптимизация: если на текущем шаге, когда стол еще не заполнен, мы имеем record + 1 квадратов,  то это решение неудачное и его можно пропускать
+ */
 fun backtracking(table : Vector<Square>, start_x : MutableInt, start_y : MutableInt, record : MutableInt,record_table : Vector<Square>, area : Int, N : Int){
 
     for (x in start_x.value until N){
@@ -199,7 +212,7 @@ fun backtracking(table : Vector<Square>, start_x : MutableInt, start_y : Mutable
         start_y.value = start_y.value.div(2)
     }
 }
-
+// главная функция, производится объявление переменных, ввод-вывод и вызов функций
 fun main() {
     val N = scan.nextInt()
     val new_N = find_koefficient(N)
